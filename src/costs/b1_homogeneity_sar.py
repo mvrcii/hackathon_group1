3a89e0a8d3f70b4d9c20583673bb38a90da709b1
Marcel Roth <MarcelRoth100@aol.com>
2025-04-10 16:54:45 +0200

    adds official b1-sar loss implementation

--- src/costs/b1_homogeneity_sar.py
+++ src/costs/b1_homogeneity_sar.py
@@ -1,28 +1,32 @@
+import numpy as np
+
 from .base import BaseCost
 from ..data.simulation import SimulationData
-from ..data.utils import B1Calculator
-from ..data.utils import SARCalculator
-
-import numpy as np
+from ..data.utils import B1Calculator, SARCalculator
 
 
 class B1HomogeneitySARCost(BaseCost):
-    def __init__(self) -> None:
+    def __init__(self,
+                 weight: float = 100) -> None:
         super().__init__()
         self.direction = "maximize"
         self.b1_calculator = B1Calculator()
         self.sar_calculator = SARCalculator()
+        self.weight = weight
 
     def calculate_cost(self, simulation_data: SimulationData) -> float:
         b1_field = self.b1_calculator(simulation_data)
         sar = self.sar_calculator(simulation_data)
-        subject = simulation_data.subject
 
-        l=1.0
+        subject = simulation_data.subject
 
         b1_field_abs = np.abs(b1_field)
         b1_field_subject_voxels = b1_field_abs[subject]
-        max_sar=np.max(sar[subject])
-        b1= np.mean(b1_field_subject_voxels) / np.std(b1_field_subject_voxels)
-        return (b1-l*max_sar)
 
+        sar_subject_voxels = sar[subject]
+
+        one_over_cov = np.mean(b1_field_subject_voxels) / np.std(b1_field_subject_voxels)
+        peak_sar = np.max(sar_subject_voxels)
+        peak_sar_sqrt = np.sqrt(peak_sar)
+        min_b1 = np.min(b1_field_subject_voxels)
+        return one_over_cov + self.weight * min_b1 / peak_sar_sqrt
