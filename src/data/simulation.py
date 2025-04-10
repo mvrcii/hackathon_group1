be363f08b13d54b4311ad51f9503be065b716191
Marcel Roth <MarcelRoth100@aol.com>
2025-04-10 13:47:57 +0200

    reset simulation code

--- src/data/simulation.py
+++ src/data/simulation.py
@@ -1,4 +1,4 @@
-
+import time
 import numpy.typing as npt
 import numpy as np
 import h5py
@@ -6,22 +6,22 @@ import os
 import einops
 
 from typing import Tuple
-from .dataclasses import SimulationRawData, SimulationData, CoilConfig
-
+from src.data.dataclasses import SimulationRawData, SimulationData, CoilConfig
 
 
 class Simulation:
-    def __init__(self, 
+    def __init__(self,
                  path: str,
                  coil_path: str = "data/antenna/antenna.h5"):
         self.path = path
         self.coil_path = coil_path
-        
+
         self.simulation_raw_data = self._load_raw_simulation_data()
-        
+        self.subject_indices = np.where(self.simulation_raw_data.subject)
+
     def _load_raw_simulation_data(self) -> SimulationRawData:
         # Load raw simulation data from path
-        
+
         def read_field() -> Tuple[npt.NDArray[np.float32], npt.NDArray[np.float32]]:
             with h5py.File(self.path) as f:
                 re_efield, im_efield = f["efield"]["re"][:], f["efield"]["im"][:]
@@ -33,18 +33,18 @@ class Simulation:
             with h5py.File(self.path) as f:
                 physical_properties = f["input"][:]
             return physical_properties
-        
+
         def read_subject_mask() -> npt.NDArray[np.bool_]:
             with h5py.File(self.path) as f:
                 subject = f["subject"][:]
             subject = np.max(subject, axis=-1)
             return subject
-        
+
         def read_coil_mask() -> npt.NDArray[np.float32]:
             with h5py.File(self.coil_path) as f:
                 coil = f["masks"][:]
             return coil
-        
+
         def read_simulation_name() -> str:
             return os.path.basename(self.path)[:-3]
 
@@ -55,18 +55,21 @@ class Simulation:
             subject=read_subject_mask(),
             coil=read_coil_mask()
         )
-        
+
         return simulation_raw_data
-    
-    def _shift_field(self, 
-                     field: npt.NDArray[np.float32], 
-                     phase: npt.NDArray[np.float32], 
+
+    def _shift_field(self,
+                     field: npt.NDArray[np.float32],
+                     phase: npt.NDArray[np.float32],
                      amplitude: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
         """
         Shift the field calculating field_shifted = field * amplitude (e ^ (phase * 1j)) and summing over all coils.
         """
+        # Calculate phase coefficients
         re_phase = np.cos(phase) * amplitude
         im_phase = np.sin(phase) * amplitude
+
+        # Create coefficients arrays
         coeffs_real = np.stack((re_phase, -im_phase), axis=0)
         coeffs_im = np.stack((im_phase, re_phase), axis=0)
         coeffs = np.stack((coeffs_real, coeffs_im), axis=0)
@@ -75,12 +78,8 @@ class Simulation:
         return field_shift
 
     def phase_shift(self, coil_config: CoilConfig) -> SimulationData:
-        # TODO only shift the subject (using mask??)
-        # TODO: get the mask (subject) from the data
-        # TODO: Then only use the relevant data for field shift
-        # TODO: Process the masked part in parallel with multiprocessing ?
         field_shifted = self._shift_field(self.simulation_raw_data.field, coil_config.phase, coil_config.amplitude)
-        
+
         simulation_data = SimulationData(
             simulation_name=self.simulation_raw_data.simulation_name,
             properties=self.simulation_raw_data.properties,
@@ -89,6 +88,31 @@ class Simulation:
             coil_config=coil_config
         )
         return simulation_data
-    
+
     def __call__(self, coil_config: CoilConfig) -> SimulationData:
-        return self.phase_shift(coil_config)
+        simulation_data = self.phase_shift(coil_config)
+
+        return simulation_data
+
+
+if __name__ == '__main__':
+    simulation = Simulation("data/simulations/children_0_tubes_2_id_19969.h5")
+    default_config = CoilConfig()
+
+    num_runs = 50
+    simulation_times = []  # List to store each run's elapsed time
+
+    # Run the simulation multiple times and record the time taken for each run.
+    for _ in range(num_runs):
+        start_time = time.perf_counter()
+        default_sim_data = simulation(default_config)
+        iteration_time = time.perf_counter() - start_time
+        print(f"Iteration {_ + 1}: {iteration_time:.6f} seconds")
+        simulation_times.append(iteration_time)
+
+    # Calculate total and average times.
+    total_time = sum(simulation_times)
+    average_time = total_time / num_runs
+
+    print(f"Total simulation time for '{simulation.simulation_raw_data.simulation_name}': {total_time:.6f} seconds")
+    print(f"Average simulation time per run: {average_time:.6f} seconds")
